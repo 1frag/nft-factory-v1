afec49a70129069735d344b8ae28c298020b7d20
Aleksey Piskunov <piskunov.alesha@gmail.com>
2022-12-31 18:41:27 +0700

    Run tests automatically

--- test/ComposableBuilderV1.test.js
+++ test/ComposableBuilderV1.test.js
@@ -2,9 +2,13 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 async function deploy () {
-    const GoodMetadataRepository = await ethers.getContractFactory('GoodMetadataRepository');
-    const goodMetadataRepository = await GoodMetadataRepository.deploy();
-    await goodMetadataRepository.deployed();
+    const Test = await ethers.getContractFactory('TestERC721');
+    const test = await Test.deploy();
+    await test.deployed();
+
+    const TestGoodMetadataRepository = await ethers.getContractFactory('TestGoodMetadataRepository');
+    const testGoodMetadataRepository = await TestGoodMetadataRepository.deploy(test.address, 3);
+    await testGoodMetadataRepository.deployed();
 
     const BuilderV1 = await ethers.getContractFactory('BuilderV1');
     const builderV1 = await BuilderV1.deploy();
@@ -18,18 +22,23 @@ async function deploy () {
     const builderV3 = await BuilderV3.deploy();
     await builderV3.deployed();
 
+    const BuilderV4 = await ethers.getContractFactory('BuilderV4');
+    const builderV4 = await BuilderV4.deploy();
+    await builderV4.deployed();
+
     const ComposableBuilderV1 = await ethers.getContractFactory('ComposableBuilderV1');
     const composableBuilderV1 = await ComposableBuilderV1.deploy(
-        goodMetadataRepository.address,
+        testGoodMetadataRepository.address,
         [
             builderV1.address,
             builderV2.address,
             builderV3.address,
+            builderV4.address,
         ],
     );
     await composableBuilderV1.deployed();
 
-    return [composableBuilderV1, builderV1, builderV2, builderV3, goodMetadataRepository];
+    return [composableBuilderV1, builderV1, builderV2, builderV3, builderV4, testGoodMetadataRepository];
 }
 
 it('create*', async function () {
@@ -44,4 +53,7 @@ it('create*', async function () {
 
     tx = await (await composableBuilderV1.createCondensed('test3')).wait();
     expect(tx.events.length).to.be.eq(1);
+
+    tx = await (await composableBuilderV1.multiCreate('test', 2, 2)).wait();
+    expect(tx.events.length).to.be.eq(6);
 });
